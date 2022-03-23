a8d7b8346ab8fec998a84cd805d5a84c53cf6160
Thomas <thomas.zaussnig@outlook.com>
2022-03-23 14:08:40 +0100

    Created mySQL table, adapted python script to write into DB, Started a prototype homepage

--- RaspberryPi/temp.py
+++ RaspberryPi/temp.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python
-data_log = open('/var/www/html/order.csv','r') 
-lines = data_log.readlines()
-data_log.close()
- 
-sensor_qty = 1     # how many sensor lines to read in from log
- 
-html_string = '<html>\n<body>\n<style>\ntable { \n  border-spacing: 10px;\n'
-html_string += '  border-collapse: separate;\n}\n</style>\n'
-html_string += '\n<table>\n<tr align="center">\n<th>Location</th>'
-html_string += '<th>Temp</th><th>Date</th><th>Time</th></tr>\n'
- 
-for x in range(-sensor_qty, 0, 1):
-    line = lines[x].split(',')
- 
-    html_string += '<tr><td align="right">'
-    html_string += line[0]
-    html_string += '</td><td align="right">'
-    html_string += line[1]
-    html_string += '&deg;C </td><td>'
-    html_string += line[4]
-    html_string += '</td><td> '
-    html_string += line[5]
-    html_string += '</td></tr>\n'
- 
-html_string += '</table>\n\n</body>\n</html>'
- 
-html_file = open('/var/www/html/temps.html','w') 
-html_file.write(html_string)
-html_file.close()
+import MySQLdb
+import sys
+
+def db_connect():
+    global cur
+    global con
+    con = MySQLdb.connect(host="localhost", user="root", password="")
+    cur = con.cursor()
+    cur.execute("USE SENSORS;")
+
+def write_data():
+    data_log = open('/var/www/html/order.csv','r') 
+    lines = data_log.readlines()
+    data_log.close()
+    sensor_qty = 1 
+    for x in range(-sensor_qty, 0, 1):
+        line = lines[x].split(',')
+        print(lines[x])
+        v1 = line[0]
+        print(line[0])
+        v2 = line[1]
+        print(line[1])
+        v3 = line[4]
+        print(line[4])
+        v4 = line[5]
+        print(line[5])
+        cur.execute("INSERT INTO metrics (location, temp, datum, time) VALUES (%s, %s, %s, %s)", (v1, v2, v3, v4))
+        con.commit();
+        
+db_connect()
+write_data()
