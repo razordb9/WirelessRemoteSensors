90368448a07f1efd3035b70af7c662120dad727b
Thomas <thomas.zaussnig@outlook.com>
2022-03-30 09:27:56 +0200

    Fixed logging problem, implemented __main__, fixed problem with writing data to db when wemos is offline

--- RaspberryPi/temp.py
+++ RaspberryPi/temp.py
@@ -2,18 +2,30 @@
 import logging
 import sys
 import mysql.connector
+from mysql.connector import errorcode
 
 def db_connect(hostname, username, password, dbName):
     global mycursor
     global mydb
-      
-    mydb = mysql.connector.connect (
+    
+    try:
+        mydb = mysql.connector.connect (
         host = hostname,
         user = username,
         password = password,
-        database = dbName
-    )
-    mycursor = mydb.cursor()
+        database = dbName)
+        mycursor = mydb.cursor()
+        logger.info('DB connection success')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            logger.error('Something is wrong with your user name or password')
+            exit()
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            logger.error('Database does not exist')
+            exit()
+        else:
+            logger.error(err)
+            exit() 
     
 def write_data(filepath, fileoperation):
     data_log = open(filepath, fileoperation) 
@@ -22,8 +34,8 @@ def write_data(filepath, fileoperation):
     data_log.close()
     sensor_qty = 1 
     for x in range(-sensor_qty, 0, 1):
-        line = lines[x].split(',')
         logger.info(lines[x])
+        line = lines[x].split(',')
         location = line[0]
         logger.info(line[0])
         temp = line[1]
@@ -33,21 +45,25 @@ def write_data(filepath, fileoperation):
         timeOfFile = line[5]
         logger.info(line[5])
         mycursor.execute("select time from metrics order by id desc limit 1")  
-        timeOfDB = mycursor.fetchall()
-        logger.info('timeOfDB ' + timeOfDB)
-        if(timeOfFile != timeOfDB):
+        timeOfDB = mycursor.fetchone()
+        timeOfFile = timeOfFile.rstrip('\n')
+        logger.info('timeOfDB ' + str(timeOfDB))
+        if(timeOfFile != timeOfDB[0]):
             logger.info('timeOfFile != timeOfDB -> true')
             sql = "INSERT INTO metrics (location, temp, datum, time) VALUES (%s, %s, %s, %s)"
             values = (location, temp, datum, timeOfFile)
             mycursor.execute(sql,values)
             mydb.commit();
+            
 
-LOG_FORMAT ="%(level)s %(asctime)s - %(messages)s"
-logging.basicConfig(filename = '/home/pi/temp.log',
-                        level = logging.DEBUG,
-                        format = LOG_FORMAT)
-logger = logging.getLogger()
 
-#password needed to be added 
-db_connect('localhost', 'root', PasswordNeeded, 'SENSORS')
-write_data('/var/www/html/order.csv','r')
+if __name__ == "__main__":
+    LOG_FORMAT ="%(levelname)s %(asctime)s - %(message)s"
+    logging.basicConfig(filename = '/home/pi/temp.log',
+                            format=LOG_FORMAT,
+                            level= logging.DEBUG)
+    logger = logging.getLogger()
+    logger.info('########## Start Logging ##########')
+    db_connect('localhost', 'root', 'yj_fb9-ePI', 'SENSORS')
+    write_data('/var/www/html/order.csv','r')
+    logger.info('########## Logging finished ##########')
